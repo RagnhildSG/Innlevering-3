4eecd7dadff765d0ea229b5ac1a0d6376deda07c
ivarjv <ivarjv@gmail.com>
2025-11-12 13:21:28 +0100

    Oppgave 4

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,15 +1,32 @@
 package no.hvl.dat100.oppgave4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+    public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+        try {
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
+            File fil = new File(mappe, filnavn);
+
+            if (fil.getParentFile() != null) {
+                fil.getParentFile().mkdirs();
+            }
+
+            PrintWriter writer = new PrintWriter(fil);
+            writer.print(samling.toString());
+            writer.close();
+
+            return true;
+
+        } catch (FileNotFoundException feil) {
+            System.out.println("Fil ikke funnet: " + feil.getMessage());
+            return false;
+        }
+    }
 }
+
